472cad680bc8b5d3507f7ccaf28db14587f97ae9
Kondjenim_ <marthinkondjeni@gmail.com>
2022-03-12 22:09:51 +0200

    -Add Unit and Acceptance test

---
+++ Gym.Core.Api.Tests.Acceptance/UnitTest1.cs
@@ -0,0 +1,19 @@
+// ---------------------------------------------------------------
+// Copyright (c) Marthin Thomas All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+using System;
+using Xunit;
+
+namespace Gym.Core.Api.Tests.Acceptance
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Gym.Core.Api.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,19 @@
+// ---------------------------------------------------------------
+// Copyright (c) Marthin Thomas All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+using System;
+using Xunit;
+
+namespace Gym.Core.Api.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
